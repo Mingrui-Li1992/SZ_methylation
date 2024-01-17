40fc9900907c8761066bb324ae9e4c538a1694cb
Mingrui-Li1992 <70991531+Mingrui-Li1992@users.noreply.github.com>
2024-01-17 18:08:47 +0800

    Create regional plot.R

---
+++ regional plot.R
@@ -0,0 +1,4 @@
+https://www.bioconductor.org/packages/release/bioc/html/coMET.html
+Martin, T.C., Yet, I., Tsai, PC. et al. https://doi.org/10.1186/s12859-015-0568-2 BMC Bioinformatics 16, 131 (2015).
+Martin, T.C., Erte, I., Tsai, P-C., Bell, J.T. (2014). “coMET: an R plotting package to visualize regional plots of epigenome-wide association scan results".
+coMET was used to draw the regional epigenome-wide association and DNA co-methylation patterns. 
